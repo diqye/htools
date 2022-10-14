3ddea89c72b21def01cbacfc23eef3e5e1857236
diqye <262666212@qq.com>
2022-10-14 11:27:40 +0800

    fix that info log output in stderr

--- scripts/test.hs
+++ scripts/test.hs
@@ -4,13 +4,24 @@ import Module.Log(hInfo,info')
 import Data.Text (Text)
 import Language.Haskell.TH
 import System.IO
-import GHC.Generics
 import Data.Aeson
 import Control.Monad
 import qualified Module.Test as Test
+import Module.TemplateGetter
 
+data Foo a = Foo
+  { field_1 :: String
+  , field_2:: a
+  } deriving Show
+
+foo = Foo "ddd" 123
+mkSetAndOver ''Foo
 
 main = do
-  Test.testLog'
+  -- Test.testLog'
+  putStrLn "11" >> $info' "hello"
+  let foo' = set_field_2 0 
+        $ set_field_1 "000"
+        $ foo
+  print foo'
   pure ()
-
